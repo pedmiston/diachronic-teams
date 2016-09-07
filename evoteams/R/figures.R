8e46372d66e1cee5754c7294f6d1dd30244a20b0
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-09-06 20:59:00 -0500

    Throw an error if figure isn't found

--- evoteams/R/figures.R
+++ evoteams/R/figures.R
@@ -1,6 +1,8 @@
 find_figure <- function(name, ext) {
   figure_file <- paste0(name, ext)
-  system.file("extdata", figure_file, package = "evoteams")
+  figure_path <- system.file("extdata", figure_file, package = "evoteams")
+  if (!file.exists(figure_path)) stop("File does not exist")
+  figure_path
 }
 
 #' Returns a path to a graphviz file included in this package
--- evoteams/tests/testthat/test_figures.R
+++ evoteams/tests/testthat/test_figures.R
@@ -6,3 +6,7 @@ test_that("find graphviz returns full path", {
   path <- find_graphviz("team-structure-single-project")
   expect_true(file.exists(path))
 })
+
+test_that("find graphviz fails if the file wasn't found", {
+  expect_error(find_graphviz("not-a-real-file"))
+})
