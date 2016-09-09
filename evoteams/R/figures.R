7da98b77a48fbb1650d742aa8c60fec719050b1b
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-09-09 11:46:34 -0500

    Pulled out functions to separate crotchet package

--- evoteams/R/figures.R
+++ evoteams/R/figures.R
@@ -1,22 +1,10 @@
-find_figure <- function(name, ext) {
-  figure_file <- paste0(name, ext)
-  figure_path <- system.file("extdata", figure_file, package = "evoteams")
-  if (!file.exists(figure_path)) stop("File does not exist")
-  figure_path
-}
-
-#' Returns a path to a graphviz file included in this package
-#' @export
-find_graphviz <- function(name) {
-  find_figure(name, ext = ".gv")
-}
-
 #' @importFrom knitr read_chunk
+#' @importFrom crotchet find_graphviz
 #' @export
-read_all_graphviz_chunks <- function() {
+read_evoteams_chunks <- function() {
   graphviz_chunks <- c("team-structure-single-project", "team-structure-multiple-projects")
   for (chunk in graphviz_chunks) {
-    chunk_path <- find_graphviz(chunk)
+    chunk_path <- find_graphviz(chunk, package = "evoteams")
     read_chunk(chunk_path, labels = chunk)
   }
 }
