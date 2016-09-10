979586038f0dcbcd40b102395e9bea552515bbf0
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-09-10 00:05:32 -0500

    Include reports in task collection

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,4 +1,4 @@
 from invoke import Collection
-from . import kaggle, evoteams
+from . import kaggle, evoteams, reports
 
-namespace = Collection(kaggle, evoteams)
+namespace = Collection(kaggle, evoteams, reports)
