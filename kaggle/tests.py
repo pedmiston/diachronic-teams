21336314cf24f510eba0d6862117acb31970b704
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-09-12 22:36:04 -0500

    Add py.test

---
+++ kaggle/tests.py
@@ -0,0 +1,9 @@
+from .leaderboards import get_leaderboard
+
+
+def test_leaderboard_has_team_members():
+    leaderboard = get_leaderboard('datasciencebowl')
+    test_case = leaderboard.team_name == 'Happy Latern Festival'
+    assert test_case.sum() == 1, 'test case not found'
+    team_members = leaderboard.ix[test_case, 'team_members']
+    assert len(team_members.split(',')) == 5, 'incorrect number of team members'
