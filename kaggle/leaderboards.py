82571594a5c0a0337081f2f6c1c12cfe48a9ce7a
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-09-12 23:41:53 -0500

    Try to get members but having problems

--- kaggle/leaderboards.py
+++ kaggle/leaderboards.py
@@ -2,16 +2,40 @@
 import sys
 import logging
 import pandas
+import requests
+import bs4
 
 logger = logging.getLogger(__name__)
 
 
 def get_leaderboard(slug):
+    output = 'slug team_name team_members team_size entries score'.split()
     url = 'https://www.kaggle.com/c/{}/leaderboard'
-    tables = pandas.read_html(url.format(slug), header=0)
+    response = requests.get(url.format(slug))
+    html = response.content.decode()
+    # Let pandas do as much as possible
+    tables = pandas.read_html(html, header=0)
     leaderboard = tables[0]
+    leaderboard.rename(columns=slugify, inplace=True)
     leaderboard['slug'] = slug
-    return leaderboard
+
+    # Get the team members from the soup
+    teams = extract_team_members(html)
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    team_divs = [el.parent
+                 for el in soup.find_all('a', class_='team-link')]
+    teams = pandas.DataFrame({'divs': team_divs})
+    teams['team_name'] = teams.divs.apply(find_team_name)
+    teams['team_members'] = teams.divs.apply(find_team_members)
+    teams['team_size'] = teams.team_members.str.split(',').apply(len)
+
+    # Merge the teams with the leaderboard
+    leaderboard['team_name'] = (leaderboard.iloc[:, 2]
+                                           .str.split('  ')
+                                           .str.get(0))
+    leaderboard = leaderboard.merge(teams)
+
+    return leaderboard[output]
 
 
 def get_leaderboards(slugs):
@@ -24,3 +48,23 @@ def get_leaderboards(slugs):
         else:
             leaderboards.append(leaderboard)
     return pandas.concat(leaderboards, ignore_index=True)
+
+
+def slugify(name):
+    return name.lower().replace(' ', '_')
+
+
+def find_team_name(div):
+    return div.find('a', class_='team-link').text.strip()
+
+
+def find_team_members(div):
+    sep = ','
+    ul = div.find('ul', class_='team-members')
+    if ul:
+        team_members = [a.text.strip() for a in ul.find_all('a')]
+    else:
+        single_player = div.find('a', class_='single-player')
+        assert single_player, 'no team members but not a single player'
+        team_members = [single_player.text.strip()]
+    return sep.join(team_members)
--- kaggle/tests.py
+++ kaggle/tests.py
@@ -1,9 +1,8 @@
 from .leaderboards import get_leaderboard
 
-
 def test_leaderboard_has_team_members():
     leaderboard = get_leaderboard('datasciencebowl')
-    test_case = leaderboard.team_name == 'Happy Latern Festival'
+    test_case = leaderboard.team_name == 'Happy Lantern Festival'
     assert test_case.sum() == 1, 'test case not found'
-    team_members = leaderboard.ix[test_case, 'team_members']
+    team_members = leaderboard.ix[test_case, 'team_members'].tolist()[0]
     assert len(team_members.split(',')) == 5, 'incorrect number of team members'
