f0cc94d6e094f12b8a531066058d2ff2b6abb6db
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-09-18 23:29:58 -0500

    Move recoders into evoteams R pkg

---
+++ evoteams/R/recoders.R
@@ -0,0 +1,23 @@
+#' Recode team structures
+#' @import dplyr
+#' @export
+recode_team_structures <- function(frame) {
+  team_structures_map <- data_frame(
+    team_structure = team_structures,
+    team_factor = factor(team_structures, levels = team_structures,
+                         labels = str_to_title(team_structures))
+  )
+  left_join(frame, team_structures_map)
+}
+
+#' Recode feedback types
+#' @import dplyr
+#' @export
+recode_feedback_types <- function(frame) {
+  feedback_map <- data_frame(
+    feedback_type = feedback_types,
+    feedback_factor = factor(feedback_type, levels = feedback_types,
+                             labels = str_to_title(feedback_types))
+  )
+  left_join(frame, feedback_map)
+}
