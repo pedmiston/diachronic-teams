5bf311d28f86c42b929f28ceadb0aa49deb4296b
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-10-10 23:19:49 -0500

    Dropped kaggle scraper. Ouch...

--- kaggle/tests.py
+++
@@ -1,8 +0,0 @@
-from .leaderboards import get_leaderboard
-
-def test_leaderboard_has_team_members():
-    leaderboard = get_leaderboard('datasciencebowl')
-    test_case = leaderboard.team_name == 'Happy Lantern Festival'
-    assert test_case.sum() == 1, 'test case not found'
-    team_members = leaderboard.ix[test_case, 'team_members'].tolist()[0]
-    assert len(team_members.split(',')) == 5, 'incorrect number of team members'
