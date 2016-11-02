8d456c4b7e6321ce93bc9efd03dbf3eaec81f910
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-11-01 21:28:18 -0500

    Stashing first plot

--- reports/chunks/modeling.R
+++ reports/chunks/modeling.R
@@ -53,45 +53,47 @@ ggplot(preds, aes(x = player, y = value)) +
   scale_alpha_manual(values = c(0.5, 0.9))
 
 # ---- search-areas
-margin <- 0.1
-ellipse_points <- function(x0, y0, a, b) {
-  ellipse_x <- function(t) x0 + a * cos(t)
-  ellipse_y <- function(t) y0 + b * sin(t)
-  t <- seq(0, 2 * pi, length.out = 100)
-  data_frame(
-    a = ellipse_x(t),
-    b = ellipse_y(t)
-  )
-}
+ellipse_x <- function(t, x0, a) x0 + a * cos(t)
+ellipse_y <- function(t, y0, b) y0 + b * sin(t)
 
-x_axis <- linesGrob(x = c(margin, 1 - margin), y = c(margin, margin), arrow = arrow())
-y_axis <- linesGrob(x = c(margin, margin), y = c(margin, 1 - margin), arrow = arrow())
+search_areas <- list(
+  overlapping = function(player) {
+    data_frame(
+      player = player,
+      team_ability = "identical",
+      a = ellipse_x(t, 0.5, 10),
+      b = ellipse_y(t, 0.5, 10)
+    )
+  },
+  disjoint = function(player) {
+    args <- list(
+      x = list(x0 = 0.5, y0 = 0.5, a = 20, b = 5),
+      y = list(x0 = 0.5, y0 = 0.5, a = 5, b = 20)
+    )[[player]]
+    data_frame(
+      player = player,
+      team_ability = "disjoint",
+      a = ellipse_x(t, args[["x0"]], args[["a"]]),
+      b = ellipse_y(t, args[["y0"]], args[["b"]])
+    )
+  },
+  intersecting = function(player) {
+    data_frame(
+      player = player,
+      team_ability = "intersecting",
+      a = ellipse_x(t, 0.5, 10),
+      b = ellipse_y(t, 0.5, 10)
+    )
+  }
+)
 
-circle <- circleGrob(r = 0.25, x = 0.5 + margin/2, y = 0.5 + margin/2)
+spotlight_coords <- lapply(search_areas, function(area_fn) {
+    # generate areas for players x and y
+    bind_rows(area_fn('x'), area_fn('y'))
+  }) %>%
+  # bind across area_fns
+  bind_rows()
 
-identical <- gTree(x_axis, y_axis, circle)
-
-
-
-plot_axes()
-
-v1 <- 0.3
-v2 <- 0.1
-player_x <- ellipse_points(0.5 + margin, 0.25, v1, v2)
-player_y <- ellipse_points(0.25, 0.5 + margin, v2, v1)
-
-grid.newpage()
-plot_axes()
-grid.polyline(x = player_x$a, y = player_x$b)
-grid.polyline(x = player_y$a, y = player_y$b)
-
-v1 <- 0.25
-v2 <- 0.15
-sep <- 0.05
-player_x <- ellipse_points(0.5 + sep, 0.5 - sep, v1, v2)
-player_y <- ellipse_points(0.5 - sep, 0.5 + sep, v2, v1)
-
-grid.newpage()
-plot_axes()
-grid.polyline(x = player_x$a, y = player_x$b)
-grid.polyline(x = player_y$a, y = player_y$b)
+ggplot(spotlight_coords, aes(a, b, group = player)) +
+  geom_line() +
+  facet_wrap("team_ability")
