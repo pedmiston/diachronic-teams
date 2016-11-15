84ab469133b11a80e93c181dd745f8ab51e839de
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-11-15 14:34:45 -0600

    Updated the graphs to use new sqlite data

--- reports/chunks/kaggle.R
+++ reports/chunks/kaggle.R
@@ -36,13 +36,13 @@ final_submissions <- submissions %>%
 team_competitions <- tbl(db, "Teams") %>%
   select(TeamId = Id, CompetitionId)
 
+# Label team size
 team_sizes <- tbl(db, "TeamMemberships") %>%
   select(TeamId, UserId) %>%
   count(TeamId) %>%
   rename(TeamSize = n)
 
 teams <- left_join(team_competitions, team_sizes)
-
 final_submissions %<>% left_join(teams, copy = TRUE)
 
 # Create leaderboards
@@ -73,7 +73,7 @@ by_place <- top_100 %>%
   group_by(FirstPlaceTeam, Place) %>%
   summarize(
     TotalSubmissions = mean(TotalSubmissions),
-    Team = mean(TeamSize)
+    TeamSize = mean(TeamSize, na.rm = TRUE)
   )
 
 by_team_size <- top_100 %>%
@@ -118,9 +118,27 @@ ggplot(by_place, aes(Place, TotalSubmissions)) +
   base_theme +
   ggtitle("Top place teams make more submissions")
 
+# ---- relative-submissions-from-place
+submissions_relative_first_place <- top_100 %>%
+  group_by(CompetitionId) %>%
+  mutate(
+    SubmissionsToFirstPlace = TotalSubmissions - TotalSubmissions[FirstPlaceTeam == TRUE]
+  ) %>%
+  ungroup()
+
+ggplot(submissions_relative_first_place, aes(Place, SubmissionsToFirstPlace)) +
+  geom_point(aes(color = FirstPlaceTeam), alpha = default_alpha,
+             stat = "summary", fun.y = "mean") +
+  scale_x_place +
+  scale_y_continuous("submissions\nrelative to first place team",
+                     breaks = seq(-100, 10, by = 5)) +
+  scale_color_manual(values = c(colors[["submissions"]], colors[["first_place"]])) +
+  base_theme
+
 # ---- place-from-submissions
-gg_place_from_submissions <- ggplot(by_submissions, aes(entries, place)) +
-  geom_point(aes(size = pct), alpha = default_alpha, color = colors[["submissions"]]) +
+gg_place_from_submissions <- ggplot(by_submissions, aes(TotalSubmissions, Place)) +
+  geom_point(aes(size = PercentTeams), alpha = default_alpha,
+             color = colors[["submissions"]]) +
   scale_x_continuous("submissions", breaks = c(1, seq(100, 600, by = 100))) +
   scale_y_place +
   scale_size_continuous("Teams", labels = percent) +
@@ -131,9 +149,9 @@ gg_place_from_submissions <- ggplot(by_submissions, aes(entries, place)) +
   ) +
   ggtitle("Making more submissions improves place")
 
-place_mod <- lm(Place ~ entries + team_size, data = top_100)
+place_mod <- lm(Place ~ TotalSubmissions + TeamSize, data = top_100)
 
-x_preds <- expand.grid(entries = 1:200, team_size = 1)
+x_preds <- expand.grid(TotalSubmissions = 1:200, TeamSize = 1)
 y_preds <- predict(place_mod, x_preds, se = TRUE)
 preds <- cbind(x_preds, y_preds) %>%
   rename(Place = fit, SE = se.fit)
@@ -143,8 +161,8 @@ gg_place_from_submissions +
               stat = "identity", color = colors[["orange"]])
 
 # ---- teamsize-from-place
-ggplot(by_place, aes(Place, team_size)) +
-  geom_point(aes(color = first_place), alpha = default_alpha) +
+ggplot(by_place, aes(Place, TeamSize)) +
+  geom_point(aes(color = FirstPlaceTeam), alpha = default_alpha) +
   scale_x_place +
   scale_y_team_size +
   scale_color_manual(values = c(colors[["team_size"]], colors[["first_place"]])) +
@@ -153,8 +171,9 @@ ggplot(by_place, aes(Place, team_size)) +
   ggtitle("First place teams are usually bigger")
 
 # ---- place-from-teamsize
-gg_place_from_teamsize <- ggplot(by_team_size, aes(team_size, place)) +
-  geom_point(aes(size = pct), alpha = default_alpha, color = colors[["team_size"]]) +
+gg_place_from_teamsize <- ggplot(by_team_size, aes(TeamSize, Place)) +
+  geom_point(aes(size = PercentTeams), alpha = default_alpha,
+             color = colors[["team_size"]]) +
   scale_x_continuous("team size", breaks = c(1, seq(10, 40, by = 10))) +
   scale_y_place +
   scale_size_continuous("Teams", labels = percent) +
@@ -164,48 +183,33 @@ gg_place_from_teamsize <- ggplot(by_team_size, aes(team_size, place)) +
   ) +
   ggtitle("Teammates help a little")
 
-place_mod <- lm(place ~ entries + team_size, data = top_100)
+place_mod <- lm(Place ~ TotalSubmissions + TeamSize, data = top_100)
 
-x_preds <- expand.grid(entries = mean(top_100$entries), team_size = 1:5)
+max_team_size <- 5
+x_preds <- expand.grid(TotalSubmissions = mean(top_100$TotalSubmissions),
+                       TeamSize = 1:max_team_size)
 y_preds <- predict(place_mod, x_preds, se = TRUE)
 preds <- cbind(x_preds, y_preds) %>%
-  rename(place = fit, se = se.fit)
+  rename(Place = fit, SE = se.fit)
 
 gg_place_from_teamsize +
-  geom_smooth(aes(ymin = place - se, ymax = place + se), data = preds,
+  geom_smooth(aes(ymin = Place - SE, ymax = Place + SE), data = preds,
               stat = "identity", color = colors[["orange"]])
 
-# ---- additional-teammates
-max_team_size <- 4
-
-gg_place_from_teamsize %+% filter(by_team_size, team_size <= max_team_size) +
+# ---- place-from-teamsize-zoom
+gg_place_from_teamsize %+% filter(by_team_size, TeamSize <= max_team_size) +
+  geom_smooth(aes(ymin = Place - SE, ymax = Place + SE), data = preds,
+              stat = "identity", color = colors[["orange"]]) +
   scale_x_continuous("team size", breaks = 1:max_team_size) +
-  geom_text(aes(label = pct_label), nudge_y = 1.2) +
+  geom_text(aes(label = PercentTeamsLabel), nudge_y = 1.2, size = 3) +
   guides(size = "none") +
   ggtitle("By far most submissions are from individuals")
 
-# ---- relative-submissions-from-place
-submissions_relative_first_place <- top_100 %>%
-  group_by(slug) %>%
-  mutate(
-    submissions_relative_first_place = entries - entries[first_place == TRUE]
-  ) %>%
-  ungroup
-
-ggplot(submissions_relative_first_place, aes(place, submissions_relative_first_place)) +
-  geom_point(aes(color = first_place), alpha = default_alpha,
-             stat = "summary", fun.y = "mean") +
-  scale_x_place +
-  scale_y_continuous("submissions\nrelative to first place team",
-                     breaks = seq(-100, 10, by = 5)) +
-  scale_color_manual(values = c(colors[["submissions"]], colors[["first_place"]])) +
-  base_theme
-
 # ---- correlation
 place_breaks <- c(100, 50, 25, 1)
-first_place <- by_place %>% filter(place == 1) %>% as.list
-ggplot(by_place, aes(team_size, entries)) +
-  geom_point(aes(color = first_place), alpha = default_alpha, size = 2) +
+first_place <- by_place %>% filter(Place == 1) %>% as.list
+ggplot(by_place, aes(TeamSize, TotalSubmissions)) +
+  geom_point(aes(color = FirstPlaceTeam), alpha = default_alpha, size = 2) +
   scale_x_continuous("team size", breaks = 1:4) +
   scale_y_continuous("submissions", breaks = c(1, seq(5, 100, by = 5))) +
   scale_color_manual(values = c("gray", colors[["first_place"]])) +
