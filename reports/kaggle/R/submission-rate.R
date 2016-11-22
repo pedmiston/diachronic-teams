003c3fe6342c37281e30909fc82d347243b47519
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-11-21 21:38:49 -0600

    Made room for remaining plot chunks

--- reports/kaggle/R/submission-rate.R
+++ reports/kaggle/R/submission-rate.R
@@ -11,28 +11,39 @@ ggplot(top_100_places, aes(Place, TotalTime)) +
   base_theme +
   labs(title = "Submission intervals are consistent across places")
 
+# ---- prop-time-per-place
+
 # ---- time-by-submission-density
+new_density_plot <- function(leaderboards) {
+  ggplot(leaderboards, aes(TotalSubmissions, TotalTimeSec)) +
+    geom_point(color = colors[["blue"]], alpha = 0.2) +
+    geom_hline(yintercept = median(leaderboards$TotalTimeSec),
+               color = "gray") +
+    geom_vline(xintercept = median(leaderboards$TotalSubmissions),
+               color = "gray") +
+    scale_x_total_submissions +
+    make_time_scale("submission interval (days)", seq(0, 400, by = 100)) +
+    base_theme
+}
+
+new_density_plot(top_100) +
+  labs(title = "Team type density for top 100 place finishes")
+
 top_10 <- leaderboards %>%
   filter(Place <= 10) %>%
   label_place_groups()
-
-ggplot(top_10, aes(TotalSubmissions, TotalTimeSec)) +
-  geom_point(color = colors[["blue"]], alpha = 0.2) +
-  geom_hline(yintercept = median(top_10$TotalTimeSec),
-             color = "gray") +
-  geom_vline(xintercept = median(top_10$TotalSubmissions),
-             color = "gray") +
-  scale_x_total_submissions +
-  make_time_scale("submission interval (days)", seq(0, 400, by = 100)) +
-  base_theme
+new_density_plot(top_10) +
+  labs(title = "Team type density for top 10 place finishes")
 
 # ---- time-by-submission-top-100
 ggplot(top_100_places, aes(TotalSubmissions, TotalTime)) +
-  geom_point(aes(alpha = Place, color = FirstPlaceTeam)) +
-  
+  #geom_point(aes(alpha = Place, color = FirstPlaceTeam)) +
+  geom_text(aes(label = Place, alpha = Place), size = 2, check_overlap = TRUE) +
   scale_x_continuous("total submissions", breaks = c(1, seq(5, 40, by = 5))) +
   scale_color_manual(values = c("black", colors[["first_place"]])) +
-  scale_alpha_continuous(range = c(0.6, 0.2)) +
+  scale_alpha_continuous(range = c(1.0, 0.2)) +
   coord_cartesian(xlim = c(1, 35), ylim = c(0, 25 * 24 * 3600)) +
   make_time_scale("submission interval (days)", seq(0, 100, by = 5)) +
   base_theme
+
+# ---- prop-time-by-rel-submissions-top-100
