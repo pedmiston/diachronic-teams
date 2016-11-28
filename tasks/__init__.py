f710024faa918dc7a89e2800e3560ecae9226a07
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-11-28 11:22:39 -0600

    Remove kaggle analysis from this proj

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,4 +1,4 @@
 from invoke import Collection
-from . import kaggle, evoteams, reports, docs
+from . import evoteams, reports, docs
 
-namespace = Collection(kaggle, evoteams, reports, docs)
+namespace = Collection(evoteams, reports, docs)
