c1aec22f6d6c23eb65e550a927df35c863e2a471
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-12-07 08:50:15 -0600

    Make tree drawing for potion images

--- derex_boyd_2016_pnas/tree.py
+++ derex_boyd_2016_pnas/tree.py
@@ -12,6 +12,10 @@ MATCH (n:Potion)
 RETURN n.generation as generation, n.label as label
 """))
 
+potion_labels = pandas.read_csv('potion_labels.csv')
+potion_labels['image'] = 'images/' + potion_labels.image
+potions = potions.merge(potion_labels)
+
 edges = pandas.DataFrame(graph.data("""
 MATCH (material:Potion) -[r:REQUIRES]-> (result:Potion)
 RETURN material.label as potion_from, result.label as potion_to
@@ -22,7 +26,7 @@ viz = Digraph(graph_attr=dict(rankdir='BT'),
                              shape='none'))
 
 for potion in potions.itertuples():
-    viz.node(potion.label)
+    viz.node(potion.label, label = '', image = potion.image)
 
 for edge in edges.itertuples():
     viz.edge(edge.potion_to, edge.potion_from)
