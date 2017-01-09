bb6e5cda1060d4ad38781ef60173440922d1dcca
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-01-09 14:03:58 -0600

    Add basic test for checking a guess

--- bots/landscapes.py
+++ bots/landscapes.py
@@ -3,8 +3,24 @@ import py2neo
 
 
 class Landscape:
+    match_clause = 'MATCH (n) -[:REQUIRES]-> (:Item {{label: "{}"}})'
+
     def __init__(self):
         self.graph = py2neo.Graph(password=environ.get('NEO4J_PASSWORD'))
 
     def evaluate_guesses(self, guesses):
-        pass
+        return [self.evaluate_guess(guess) for guess in guesses]
+
+    def evaluate_guess(self, guess):
+        clauses = [self.match_clause.format(label) for label in guess]
+        query = '\n'.join(['MATCH (n:Item)'] + clauses + ['RETURN n;'])
+        result = self.graph.data(query)
+
+        if len(result) == 0:
+            raise NoInnovationFoundError
+
+        return result[0]['n']['label']
+
+
+class NoInnovationFoundError(Exception):
+    """A guess and a fail."""
