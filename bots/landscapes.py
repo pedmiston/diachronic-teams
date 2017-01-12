8dfdb5a4c974a348fdc695e4f6a8c8f786cb7a2f
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-01-11 21:41:19 -0600

    Better documentation of what can go wrong

--- bots/landscapes.py
+++ bots/landscapes.py
@@ -6,7 +6,11 @@ class Landscape:
     match_clause = 'MATCH (n) -[:REQUIRES]-> (:Item {{label: "{}"}})'
 
     def __init__(self):
-        self.graph = py2neo.Graph(password=environ.get('NEO4J_PASSWORD'))
+        try:
+            password = environ['NEO4J_PASSWORD']
+        except KeyError:
+            raise py2neo.Unauthorized('NEO4J_PASSWORD not set')
+        self.graph = py2neo.Graph(password=password)
 
     def evaluate_guesses(self, guesses):
         results = []
@@ -20,15 +24,32 @@ class Landscape:
         return results
 
     def evaluate_guess(self, guess):
+        """Given a guess, see if it made anything.
+
+        Args:
+            guess: A list of item labels in any order.
+        Returns:
+            An answer label (str).
+        Raises:
+            py2neo.Unauthorized: If access to the db is prevented.
+            NoInnovationFoundError: If the guess didn't make anything.
+            TooManyInnovationsFoundError: If more than one answer was found.
+        """
         clauses = [self.match_clause.format(label) for label in guess]
         query = '\n'.join(['MATCH (n:Item)'] + clauses + ['RETURN n;'])
-        result = self.graph.data(query)
+        results = self.graph.data(query)  # raises py2neo.Unauthorized
 
-        if len(result) == 0:
+        if len(results) == 0:
             raise NoInnovationFoundError
+        elif len(results) > 1:
+            raise TooManyInnovationsFoundError
 
-        return result[0]['n']['label']
+        answer = results[0]['n']
+        return answer['label']
 
 
 class NoInnovationFoundError(Exception):
     """A guess and a fail."""
+
+class TooManyInnovationsFoundError(Exception):
+    """A single guess should have a single answer."""
