c706f8c9108d2af432e6d6fe3ecf709c54016857
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-01-20 13:36:40 -0600

    Update fig depicting all conditions

--- tasks/docs.py
+++ tasks/docs.py
@@ -1,10 +1,11 @@
 from invoke import task
 from unipath import Path
 
-
 @task
-def build(ctx, name, open=True):
-    """Build the named document."""
-    dir = Path('docs', name)
-    flags = '-o' if open else ''
-    cmd = ctx.run('cd {} && ./build {}'.format(dir, flags))
+def img(ctx, name):
+    if name == 'list':
+        print('\n'.join(Path('evoteams/inst/extdata/').listdir()))
+        return
+    src = Path('evoteams/inst/extdata/{}.gv'.format(name))
+    dst = Path('img/{}.png'.format(name))
+    ctx.run('dot -Tpng -o {} {}'.format(dst, src), echo=True)
