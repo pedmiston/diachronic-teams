331eaeabb4d066831b606bcd943ae6b28838311b
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-01-23 08:33:43 -0600

    Removed evoteams/R/figures.R and evoteams/R/time.R

--- evoteams/R/figures.R
+++
@@ -1,6 +0,0 @@
-#' Read all available knitr chunks in the evoteams package.
-#' @importFrom crotchet read_all_graphviz_chunks
-#' @export
-read_evoteams_chunks <- function() {
-  read_all_graphviz_chunks("evoteams")
-}
