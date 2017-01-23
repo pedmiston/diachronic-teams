35b6e06d1366ebb35e24216d2736e30c797d7be8
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-01-23 10:47:50 -0600

    Use better dpi for png

--- tasks/docs.py
+++ tasks/docs.py
@@ -2,10 +2,10 @@ from invoke import task
 from unipath import Path
 
 @task
-def img(ctx, name, ext='png'):
+def img(ctx, name, ext='png', dpi=300):
     if name == 'list':
         print('\n'.join(Path('evoteams/inst/extdata/').listdir()))
         return
     src = Path('evoteams/inst/extdata/{}.gv'.format(name))
     dst = Path('img/{}.{}'.format(name, ext))
-    ctx.run('dot -T{} -o {} {}'.format(ext, dst, src))
+    ctx.run('dot -T{} -Gdpi={} -o {} {}'.format(ext, dpi, dst, src))
