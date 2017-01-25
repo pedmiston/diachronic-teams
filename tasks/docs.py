7c8cec838bd2b002433a88c4a19c0934a1bc46e8
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-01-24 22:37:21 -0600

    Allow setting img output name

--- tasks/docs.py
+++ tasks/docs.py
@@ -1,11 +1,11 @@
 from invoke import task
 from unipath import Path
 
-@task
-def img(ctx, name, ext='png', dpi=300):
+@task(help=dict(name='If name is "list", list available names.'))
+def img(ctx, name, output=None, ext='png', dpi=300):
     if name == 'list':
         print('\n'.join(Path('evoteams/inst/extdata/').listdir()))
         return
     src = Path('evoteams/inst/extdata/{}.gv'.format(name))
-    dst = Path('img/{}.{}'.format(name, ext))
+    dst = Path('img/{}.{}'.format(output or name, ext))
     ctx.run('dot -T{} -Gdpi={} -o {} {}'.format(ext, dpi, dst, src))
