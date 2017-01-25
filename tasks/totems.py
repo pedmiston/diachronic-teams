6f7692f6823e20a3ba32c8ef2145a1d2cdc7c8c9
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-01-25 15:12:52 -0600

    New invoke task for downloading subj info

--- tasks/totems.py
+++ tasks/totems.py
@@ -4,7 +4,9 @@ from invoke import task
 import sqlalchemy
 import ansible_vault
 import pandas
+import gspread
 from unipath import Path
+from oauth2client.service_account import ServiceAccountCredentials
 
 from . import paths
 
@@ -29,11 +31,35 @@ def download(ctx):
         out_csv = Path(TOTEMS_DIR, '{}.csv'.format(table.split('_')[1]))
         frame.to_csv(out_csv, index=False)
 
+@task
+def subj_info(ctx):
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+        get_from_vault(vault_file='secrets/lupyanlab-service-account.json'),
+        scopes='https://spreadsheets.google.com/feeds')
 
-def get_from_vault(key):
+    gc = gspread.authorize(credentials)
+
+    title = 'totems-subj-info'
+    try:
+        ws = gc.open(title).sheet1
+    except gspread.SpreadsheetNotFound:
+        print('spreadsheet %s not found, is it shared with the creds email?' % title)
+
+    df = pandas.DataFrame(ws.get_all_records())
+    df.rename(columns=dict(SubjID='ID_Player',
+                           Initials='Experimenter'),
+              inplace=True)
+    cols = 'ID_Player Strategy Date Room Experimenter Compliance'.split()
+    df[cols].to_csv(Path(TOTEMS_DIR, 'SubjInfo.csv'), index=False)
+
+
+def get_from_vault(key=None, vault_file='db/vars/secrets.yml'):
     ansible_vault_password_file = environ.get('ANSIBLE_VAULT_PASSWORD_FILE')
     ansible_vault_password = open(ansible_vault_password_file).read().strip()
     vault = ansible_vault.Vault(ansible_vault_password)
-    secrets_yaml = Path(paths.PROJ, 'db/vars/secrets.yml')
+    secrets_yaml = Path(paths.PROJ, vault_file)
     data = vault.load(open(secrets_yaml).read())
-    return data.get(key)
+    if key is None:
+        return data
+    else:
+        return data.get(key)
