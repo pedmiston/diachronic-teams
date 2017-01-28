817d32a50a2714d42b90ea3cb79ece1e6708fe2d
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-01-27 21:25:23 -0600

    Deidentify data before adding to repo

--- evoteams/data-raw/use-data-totems.R
+++ evoteams/data-raw/use-data-totems.R
@@ -10,7 +10,17 @@ assign_csvs_with_prefix("data-raw/totems", "totems_")
 totems_leaderboards <- totems_player %>%
   left_join(totems_group) %>%
   rename(Strategy = Treatment) %>%
+  select(-c(ID_Number:Gain, Knowledge, Size, Open, Status)) %>%
+  # Filter out any subjects that aren't reported
+  # in the subject info sheet.
   inner_join(totems_subjinfo)
+
+# Deidentification.
+# Remove datetime information and shuffle rows.
+set.seed(328)
+totems_leaderboards %<>%
+  select(-Date, -Room) %>%
+  sample_n(nrow(totems_leaderboards))
 
 use_data(
   totems_leaderboards,
