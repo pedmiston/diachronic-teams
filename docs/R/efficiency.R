8272223bd35790dc929396732f1d1a94746cac57
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-03-07 14:45:58 -0600

    Corrected plot showing number of unique guesses

--- docs/R/efficiency.R
+++ docs/R/efficiency.R
@@ -1,7 +1,5 @@
 source("docs/R/setup.R")
 
-
 ggplot(totems_workshops) +
   aes(TeamTime, NumUniqueGuesses, color = Strategy) +
-  geom_point(stat = "summary", fun.y = "mean") +
   geom_smooth(method = "lm", se = FALSE)
