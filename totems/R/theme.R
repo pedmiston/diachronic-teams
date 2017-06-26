0142207484f7e074dd6931bbef2bb7df226303fb
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-06-26 11:43:52 -0500

    New color picker and new gv

--- totems/R/theme.R
+++ totems/R/theme.R
@@ -4,6 +4,8 @@
 load_totems_theme <- function() {
   strategy_levels = levels(recode_strategy()$StrategyLabel)
   strategy_colors = RColorBrewer::brewer.pal(length(strategy_levels), "Set2")
+  strategy_named_colors <- strategy_colors
+  names(strategy_named_colors) <- c("green", "orange", "blue")
 
   totems_theme <- list(
     # Theme
@@ -12,6 +14,8 @@ load_totems_theme <- function() {
     synchronic_color = strategy_colors[1],
     isolated_color = strategy_colors[2],
     diachronic_color = strategy_colors[3],
+    color_picker = function(x) unname(strategy_named_colors[x]),
+
     # Strategy scales
     scale_x_strategy = ggplot2::scale_x_discrete("Strategy", labels = strategy_levels),
     scale_color_strategy = ggplot2::scale_color_manual("Strategy", values = strategy_colors),
