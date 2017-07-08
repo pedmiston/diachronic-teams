1c2da406cbad8f1b7f2cfcc97329aa1f09df2b8f
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-07-08 06:37:15 -0500

    Combine replication with calendar hours/labor hours

--- docs/simulations/R/strategy.R
+++ docs/simulations/R/strategy.R
@@ -3,9 +3,15 @@ source("docs/simulations/R/setup.R")
 # ---- strategy
 data("BotsStrategy")
 
+BotsStrategy %<>%
+  mutate(strategy_rev = factor(strategy, levels = c("synchronic", "diachronic")))
+
 timeline_plot <- ggplot(BotsStrategy) +
   aes(round, inventory_size, color = strategy) +
   geom_line(stat = "summary", fun.y = "mean", size = 1.2) +
+  geom_vline(xintercept = 49, linetype = 2, size = 0.3) +
+  annotate("text", x = 49, y = 14, label = "calendar hours", angle = 90,
+           vjust = -0.2, size = 3) +
   scale_x_continuous("Round") +
   totems_theme$scale_y_inventory_size +
   scale_color_manual(values = totems_theme$color_picker(c("blue", "green"))) +
@@ -13,11 +19,7 @@ timeline_plot <- ggplot(BotsStrategy) +
   totems_theme$base_theme +
   theme(legend.position = "top")
 
-BotsStrategyFinal <- BotsStrategy %>%
-  filter_final_round() %>%
-  mutate(strategy_rev = factor(strategy, levels = c("synchronic", "diachronic")))
-
-final_plot <- ggplot(BotsStrategyFinal) +
+final_plot <- ggplot() +
   aes(strategy_rev, inventory_size) +
   geom_bar(aes(fill = strategy), stat = "summary", fun.y = "mean",
            alpha = 0.6) +
@@ -34,10 +36,20 @@ final_plot <- ggplot(BotsStrategyFinal) +
     panel.grid.major.x = element_blank()
   )
 
+calendar_hours_plot <- final_plot %+% filter(BotsStrategy, round == 49)
+labor_hours_plot    <- final_plot %+% filter_final_round(BotsStrategy)
+
 gridExtra::grid.arrange(
-  crotchet::read_graphviz("labor-hours", "totems"),
-  timeline_plot + theme(legend.position = "none"),
-  final_plot + theme(axis.title.y = element_blank()),
+  crotchet::read_graphviz("team-structures-2", "totems"),
+  timeline_plot +
+    theme(legend.position = "none") +
+    ggtitle("Performance over time"),
+  calendar_hours_plot +
+    theme(axis.title.y = element_blank()) +
+    ggtitle("Calendar hours"),
+  labor_hours_plot +
+    theme(axis.title.y = element_blank()) +
+    ggtitle("Labor hours"),
   nrow = 1,
-  widths = c(0.25, 0.375, 0.375)
+  widths = c(0.16, 0.28, 0.28, 0.28)
 )
