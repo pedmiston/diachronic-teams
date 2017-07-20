ad8480db7b1afea4bdafa2b094c06bb428958fe7
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-07-20 13:30:17 -0500

    Switch from measuring lag to measuring balance

--- docs/R/invention-times.R
+++ docs/R/invention-times.R
@@ -18,19 +18,31 @@ InventionTimes <- Guesses %>%
   arrange(TeamID, Result, TeamTime) %>%
   select(TeamID, InventionID = Result, PlayerIX, Time = TeamTime) %>%
   spread(PlayerIX, Time) %>%
-  mutate(P1_minus_P2 = P1 - P2)
+  filter(!(is.na(P1) | is.na(P2))) %>%
+  mutate(
+    P1_minus_P2 = P1 - P2,
+    FirstPlayer = ifelse(P1 > P2, "P1", "P2")
+  )
 
-team_lags_plot <- ggplot(InventionTimes) +
-  aes(P1_minus_P2) +
-  geom_dotplot(binwidth=60) +
+team_balance_plot <- ggplot(InventionTimes) +
+  aes(FirstPlayer) +
+  geom_histogram(stat = "count") +
   facet_wrap("TeamID")
 
-PerformanceByLag <- InventionTimes %>%
+PerformanceByBalance <- InventionTimes %>%
   group_by(TeamID) %>%
-  summarize(Lag = mean(abs(P1_minus_P2), na.rm = TRUE)) %>%
+  summarize(Balance = sum(FirstPlayer == "P1")/n()) %>%
   left_join(TeamPerformance, .)
 
-performance_by_lag_plot <- ggplot(PerformanceByLag) +
-  aes(Lag, NumInnovations) +
+performance_by_balance_mod <- lm(NumInnovations ~ Balance,
+                                 data = PerformanceByBalance)
+
+performance_by_balance_preds <- data_frame(Balance = seq(0, 1, by = 0.05)) %>%
+  cbind(., predict(performance_by_balance_mod, newdata = ., se = TRUE)) %>%
+  rename(NumInnovations = fit, SE = se.fit)
+
+performance_by_balance_plot <- ggplot(PerformanceByBalance) +
+  aes(Balance, NumInnovations) +
   geom_point() +
-  geom_smooth(method = "lm")
+  geom_smooth(aes(ymin = NumInnovations - SE, ymax = NumInnovations + SE),
+              data = performance_by_balance_preds, stat = "identity")
