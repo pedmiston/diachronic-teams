9b90cc6af3b3b7ccb57610ce773320582c6535ae
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-11-06 12:11:37 -0600

    Getting closer on timecourse plots

--- docs/R/counts.R
+++ docs/R/counts.R
@@ -1,9 +1,17 @@
 source("docs/R/setup.R")
 
 # ---- counts
+TeamSizes <- Teams %>%
+  select(Exp, Strategy, SessionDuration, PlayersPerTeam = NumPlayers, SessionsPerPlayer) %>%
+  unique()
+
 condition_counts <- Teams %>%
   filter(TeamStatus == "V") %>%
   count(Exp, Strategy, SessionDuration) %>%
-  arrange(desc(Exp), Strategy)
+  arrange(desc(Exp), Strategy) %>%
+  rename(NumTeams = n) %>%
+  left_join(TeamSizes) %>%
+  mutate(NumPlayers = PlayersPerTeam * NumTeams) %>%
+  select(Exp, Strategy, PlayersPerTeam, SessionDuration, SessionsPerPlayer, NumTeams, NumPlayers)
 
 condition_counts
--- docs/R/timecourse.R
+++ docs/R/timecourse.R
@@ -1,21 +1,37 @@
 source("docs/R/setup.R")
 
+# ---- timecourse-50-teams
+data("Sampled")
+
+SampledTeamMax <- Sampled %>%
+  filter(
+    TeamStatus == "V",
+    Exp == "50LaborMinutes"
+  ) %>%
+  group_by(TeamID, Strategy, SessionDuration, Generation, SampledSessionTime) %>%
+  summarize(MaxInnovations = max(TeamInventorySize)) %>%
+  ungroup() %>%
+  recode_strategy()
+
+ggplot(filter(SampledTeamMax, Strategy == "Synchronic")) +
+  aes(SampledSessionTime, MaxInnovations, group = TeamID) +
+  geom_line()
+
 # ---- timecourse-50
 data("Sampled")
 
 SampledMeans <- Sampled %>%
   filter(
     TeamStatus == "V",
-    SessionStatus == "V",
     Exp == "50LaborMinutes"
   ) %>%
-  group_by(TeamID, Strategy, SessionDuration, Generation, SampledSessionTime) %>%
+  group_by(TeamID, Strategy, SessionDuration, Generation, TeamTime) %>%
   summarize(NumInnovations = mean(TeamInventorySize)) %>%
   ungroup() %>%
   recode_strategy()
 
 ggplot(SampledMeans) +
-  aes(SampledSessionTime, NumInnovations,
-      group = interaction(StrategyLabel, SessionDuration),
+  aes(TeamTime, NumInnovations,
+      group = interaction(StrategyLabel, SessionDuration, Generation),
       color = StrategyLabel, linetype = factor(SessionDuration)) +
   geom_line(stat = "summary", fun.y = "mean")
