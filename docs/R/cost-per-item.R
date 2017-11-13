bbe18e330badcc7b6a95c8172aa2589c0cce1c40
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-11-13 15:15:21 -0600

    Add results of cost per item analysis

---
+++ docs/R/cost-per-item.R
@@ -0,0 +1,158 @@
+# ---- cost-per-item
+data("Guesses")
+data("AdjacentItems")
+data("Teams")
+
+recode_discovered <- function(frame) {
+  levels <- c(TRUE, FALSE)
+  types <- c("discovered", "undiscovered")
+  labels <- c("Discovered", "Undiscovered")
+  long_labels <- c("Item discovered", "Item never discovered")
+  map <- data_frame(
+    Discovered = levels,
+    DiscoveredType = types,
+    DiscoveredLabel = factor(levels, levels = levels, labels = labels),
+    DiscoveredLong = factor(levels, levels = levels, labels = long_labels)
+  )
+  if(missing(frame)) return(map)
+  left_join(frame, map)
+}
+
+CostPerItem <- Guesses %>%
+  # Copy guesses for each adjacent item
+  left_join(AdjacentItems, by = c("PrevSessionInventoryID" = "ID")) %>%
+  group_by(Exp, TeamID, Adjacent) %>%
+  summarize(
+    TotalGuesses = n(),
+    Discovered = any(Result == Adjacent)
+  ) %>%
+  ungroup() %>%
+  left_join(Teams) %>%
+  recode_discovered() %>%
+  recode_strategy()
+
+scale_x_adjacent <- scale_x_discrete("Innovation ID")
+
+# 50: Guesses per item ----
+TeamCostPerItem50 <- CostPerItem %>%
+  filter(Exp == "50LaborMinutes", TeamStatus == "V") %>%
+  mutate(AdjacentFactor = factor(Adjacent))
+
+guesses_per_item_50_plot <- ggplot(TeamCostPerItem50) +
+  aes(AdjacentFactor, TotalGuesses, group = Strategy) +
+  geom_bar(aes(fill = StrategyLabel),
+           stat = "summary", fun.y = "mean", position = "dodge") +
+  scale_x_adjacent +
+  totems_theme$scale_fill_strategy +
+  totems_theme$base_theme +
+  guides(fill = guide_legend(reverse = TRUE)) +
+  theme(legend.position = "top")
+
+guesses_per_item_50_mod <- lmer(TotalGuesses ~ Diachronic_v_Isolated + Diachronic_v_Synchronic +
+       (Diachronic_v_Isolated + Diachronic_v_Synchronic|Adjacent),
+     data = TeamCostPerItem50)
+
+guesses_per_item_50_preds <- recode_strategy() %>%
+  cbind(., predictSE(guesses_per_item_50_mod, newdata = ., se = TRUE)) %>%
+  rename(TotalGuesses = fit, SE = se.fit)
+
+guesses_per_item_50_mod_plot <- ggplot(guesses_per_item_50_preds) +
+  aes(StrategyLabel, TotalGuesses) +
+  geom_bar(aes(fill = StrategyLabel), stat = "identity") +
+  geom_linerange(aes(ymin = TotalGuesses - SE, ymax = TotalGuesses + SE)) +
+  scale_y_continuous("Average guesses per item") +
+  totems_theme$scale_x_strategy +
+  totems_theme$scale_fill_strategy +
+  totems_theme$base_theme +
+  theme(legend.position = "none")
+
+# 50: Guesses per item by discovery ----
+guesses_per_item_50_by_discovery_plot <- guesses_per_item_50_plot +
+  facet_wrap("DiscoveredLabel", nrow = 2, scales = "free_y")
+
+guesses_per_discovered_item_50_mod <- lmer(
+  TotalGuesses ~ Diachronic_v_Isolated + Diachronic_v_Synchronic +
+    (Diachronic_v_Isolated + Diachronic_v_Synchronic|Adjacent),
+  data = filter(TeamCostPerItem50, Discovered)
+)
+guesses_per_discovered_item_50_mod_preds <- recode_strategy() %>%
+  cbind(., predictSE(guesses_per_discovered_item_50_mod, newdata = ., se = TRUE)) %>%
+  rename(TotalGuesses = fit, SE = se.fit)
+
+guesses_per_undiscovered_item_50_mod <- lmer(
+  TotalGuesses ~ Diachronic_v_Isolated + Diachronic_v_Synchronic +
+    (Diachronic_v_Isolated + Diachronic_v_Synchronic|Adjacent),
+  data = filter(TeamCostPerItem50, !Discovered)
+)
+guesses_per_undiscovered_item_50_mod_preds <- recode_strategy() %>%
+  cbind(., predictSE(guesses_per_undiscovered_item_50_mod, newdata = ., se = TRUE)) %>%
+  rename(TotalGuesses = fit, SE = se.fit)
+
+guesses_per_item_50_by_discovered_preds <- bind_rows(
+  discovered = guesses_per_discovered_item_50_mod_preds,
+  undiscovered = guesses_per_undiscovered_item_50_mod_preds,
+  .id = 'DiscoveredType'
+) %>%
+  recode_discovered()
+
+guesses_per_item_50_by_discovered_mod_plot <- (guesses_per_item_50_mod_plot %+% guesses_per_item_50_by_discovered_preds) +
+  facet_wrap("DiscoveredLabel", ncol = 1, scales = "free_y")
+
+# 100: Guesses per item ----
+TeamCostPerItem100 <- CostPerItem %>%
+  filter(Exp == "100LaborMinutes", TeamStatus == "V") %>%
+  filter(!is.na(Adjacent), !is.na(Discovered)) %>%
+  mutate(AdjacentFactor = factor(Adjacent))
+
+guesses_per_item_100_plot <- (guesses_per_item_50_plot %+% TeamCostPerItem100)
+
+guesses_per_item_100_mod <- lmer(
+  TotalGuesses ~ Diachronic_v_Isolated + Diachronic_v_Synchronic +
+    (Diachronic_v_Isolated + Diachronic_v_Synchronic|Adjacent),
+  data = filter(TeamCostPerItem100, Discovered == TRUE))
+
+guesses_per_item_100_preds <- recode_strategy() %>%
+  cbind(., predictSE(guesses_per_item_100_mod, newdata = ., se = TRUE)) %>%
+  rename(TotalGuesses = fit, SE = se.fit)
+
+guesses_per_item_100_mod_plot <- ggplot(guesses_per_item_100_preds) +
+  aes(StrategyLabel, TotalGuesses) +
+  geom_bar(aes(fill = StrategyLabel), stat = "identity") +
+  geom_linerange(aes(ymin = TotalGuesses - SE, ymax = TotalGuesses + SE)) +
+  scale_y_continuous("Average guesses per item") +
+  totems_theme$scale_x_strategy +
+  totems_theme$scale_fill_strategy +
+  totems_theme$base_theme +
+  theme(legend.position = "none")
+
+# 100: Guesses per item by discovered ----
+guesses_per_item_100_by_discovery_plot <- guesses_per_item_100_plot +
+  facet_wrap("DiscoveredLabel", nrow = 2, scales = "free_y")
+
+guesses_per_discovered_item_100_mod <- lmer(
+  TotalGuesses ~ Diachronic_v_Isolated + Diachronic_v_Synchronic +
+    (Diachronic_v_Isolated + Diachronic_v_Synchronic|Adjacent),
+  data = filter(TeamCostPerItem100, Discovered)
+)
+guesses_per_discovered_item_100_mod_preds <- recode_strategy() %>%
+  cbind(., predictSE(guesses_per_discovered_item_100_mod, newdata = ., se = TRUE)) %>%
+  rename(TotalGuesses = fit, SE = se.fit)
+
+guesses_per_undiscovered_item_100_mod <- lmer(
+  TotalGuesses ~ Diachronic_v_Isolated + Diachronic_v_Synchronic +
+    (Diachronic_v_Isolated + Diachronic_v_Synchronic|Adjacent),
+  data = filter(TeamCostPerItem100, !Discovered)
+)
+guesses_per_undiscovered_item_100_mod_preds <- recode_strategy() %>%
+  cbind(., predictSE(guesses_per_undiscovered_item_100_mod, newdata = ., se = TRUE)) %>%
+  rename(TotalGuesses = fit, SE = se.fit)
+
+guesses_per_item_100_by_discovered_preds <- bind_rows(
+  discovered = guesses_per_discovered_item_100_mod_preds,
+  undiscovered = guesses_per_undiscovered_item_100_mod_preds,
+  .id = 'DiscoveredType'
+) %>%
+  recode_discovered()
+
+guesses_per_item_100_by_discovered_mod_plot <- (guesses_per_item_100_mod_plot %+% guesses_per_item_100_by_discovered_preds) +
+  facet_wrap("DiscoveredLabel", ncol = 1, scales = "free_y")
