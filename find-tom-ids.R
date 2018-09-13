9a861808e90daece0b5a7b8afc1919777eab72ef
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-09-13 09:48:44 -0500

    Simple script for extracting IDs from TOM subjects

---
+++ find-tom-ids.R
@@ -0,0 +1,23 @@
+# Find TOM IDs to use as starting points for new diachronic subjects.
+library(tidyverse)
+library(totems)
+data("Sessions")
+
+valid_tom_player_ids <- Sessions %>%
+  filter_selfother() %>%
+  filter(Strategy == "Isolated") %>%
+  count(PlayerID) %>%
+  filter(n == 4) %>%
+  .$PlayerID
+
+n_valid_tom <- length(valid_tom_player_ids)
+
+new_ids <- Sessions %>%
+  filter(
+    PlayerID %in% valid_tom_player_ids,
+    SessionIX < 4
+  ) %>%
+  mutate(AncestorID = as.numeric(str_replace(SessionID, "S", ""))) %>%
+  .$AncestorID
+
+write(paste(new_ids, collapse = "\n"), "tom-ids.txt")
